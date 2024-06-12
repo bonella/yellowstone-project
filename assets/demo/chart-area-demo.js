62a004382cf66df0dc0cc78d0b4d73cfba81ae52
ThielG <guilhermethiel165@gmail.com>
2024-06-12 20:41:40 -0300

    new-front

--- assets/demo/chart-area-demo.js
+++ assets/demo/chart-area-demo.js
@@ -7,20 +7,20 @@ var ctx = document.getElementById("myAreaChart");
 var myLineChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ["Mar 1", "Mar 2", "Mar 3", "Mar 4", "Mar 5", "Mar 6", "Mar 7", "Mar 8", "Mar 9", "Mar 10", "Mar 11", "Mar 12", "Mar 13"],
+    labels: ["01/03", "02/03", "02/03", "03/03", "04/03", "05/03", "06/03", "07/03", "08/03", "09/03", "10/03", "12/03", "13/03"],
     datasets: [{
-      label: "Sessions",
+      label: "Gastos",
       lineTension: 0.3,
-      backgroundColor: "rgba(2,117,216,0.2)",
-      borderColor: "rgba(2,117,216,1)",
+      backgroundColor: "#f7d7a874",
+      borderColor: "#ea9b25",
       pointRadius: 5,
-      pointBackgroundColor: "rgba(2,117,216,1)",
+      pointBackgroundColor: "#ea9b25",
       pointBorderColor: "rgba(255,255,255,0.8)",
       pointHoverRadius: 5,
-      pointHoverBackgroundColor: "rgba(2,117,216,1)",
+      pointHoverBackgroundColor: "#ea9b25",
       pointHitRadius: 50,
       pointBorderWidth: 2,
-      data: [10000, 30162, 26263, 18394, 18287, 28682, 31274, 33259, 25849, 24159, 32651, 31984, 38451],
+      data: [10000, 30162, 26263, 18394, 18087, 28682, 31274, 31259, 22849, 24159, 32651, 31284, 31451],
     }],
   },
   options: {
