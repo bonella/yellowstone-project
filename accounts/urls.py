9f69612293650b0f105798f7242c8698b685645f
HenriquePrudencio <141193621+HenriquePrudencio@users.noreply.github.com>
2024-06-26 20:36:28 -0300

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('', views.login, name='login'),
+    path('login/', views.login, name='login'),
     path('plataforma/', views.plataforma, name='plataforma'),
     path('password/', views.password, name='password'),
 ]
